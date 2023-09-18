2ffa13b3d85359e15ae41871ae0d80e3ce6ccc35
Rachael <racsmith277@gmail.com>
2023-09-18 08:02:42 -0400

    welcome section

---
+++ app/_content/Welcome.tsx
@@ -0,0 +1,28 @@
+import { GithubIcon, LinkedInIcon, TwitterIcon } from "@/components/Icons";
+import Link from "next/link";
+
+function Welcome() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center border-b border-black bg-neutral-100">
+      <div className="flex max-w-md flex-col gap-6 items-center">
+        <h1 className="text-center font-heading text-3xl">{`Hi, I'm Rachael.`}</h1>
+        <p className="font-body text-neutral-400 text-center">{`I’m a founder & full stack design engineer who loves building products from zero to one.`}</p>
+        <div className='w-48'>
+        <div className='flex justify-around'>
+          <Link href="https://twitter.com/witty_genstein" target="_blank">
+            <TwitterIcon />
+          </Link>
+          <Link href="https://www.linkedin.com/in/rachael-smith-101/" target="_blank">
+            <LinkedInIcon />
+          </Link>
+          <Link href="https://github.com/racsmith277" target="_blank">
+            <GithubIcon />
+          </Link>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default Welcome;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import Welcome from '@/app/_content/Welcome'
+import About from './_content/About'
 
 export default function Home() {
   return (
     <main className="">
-      
+      <Welcome />      
     </main>
   )
 }
