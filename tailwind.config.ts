76ffb27aa3880c65d726edff5b53bd66bb1fc89d
Rachael <racsmith277@gmail.com>
2023-09-18 07:38:28 -0400

    fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,10 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      heading: ['var(--font-krungthep)', 'serif'],
+      body:['var(--font-basier)', 'mono']
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
