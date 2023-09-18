d451a6bc614106acf25730dc92191b2575251acc
Rachael <racsmith277@gmail.com>
2023-09-18 07:39:04 -0400

    remove default content

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,11 +12,6 @@ const config: Config = {
       body:['var(--font-basier)', 'mono']
     },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
   },
   plugins: [],
